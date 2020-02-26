807a34f9e09c4ee9a631b14c11986431137515c4
Michael Dirolf <mike@dirolf.com>
2020-02-25 22:00:01 -0500

    Flex

--- crossword/src/Page.tsx
+++ crossword/src/Page.tsx
@@ -15,11 +15,12 @@ interface SquareAndColsProps {
 export const SquareAndCols = (props: SquareAndColsProps) => {
   return (
     <div css={{
-      textAlign: 'center',
+      display: 'flex',
+      flexWrap: 'wrap',
+      alignItems: 'end',
     }}>
       <div css={{
         padding: '10px',
-        display: 'inline-block',
         height: 'calc(min(60vh - 84px, 100vw))',
         width: 'calc(min(60vh - 84px, 100vw))',
         '@media (min-width: 576px)': {
@@ -32,41 +33,39 @@ export const SquareAndCols = (props: SquareAndColsProps) => {
         },
       }}>
         {props.square}
-    </div>
+      </div>
       <div css={{
-        float: 'right',
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'end',
         width: '100vw',
+        height: '40vh',
         '@media (min-width: 576px)': {
           width: '34vw',
+          height: 'calc(min(100vh - 84px, 66vw))',
         },
         '@media (min-width: 992px)': {
           width: '50vw',
+          height: 'calc(min(100vh - 84px, 50vw))',
+
         },
       }}>
         <div css={{
           padding: '10px',
-          display: 'inline-block',
           width: '100%',
-          height: '20vh',
-          '@media (min-width: 576px)': {
-            height: 'calc(50vh - 42px)',
-          },
+          height: '50%',
           '@media (min-width: 992px)': {
             width: '50%',
-            height: 'calc(100vh - 84px)',
+            height: '100%',
           },
         }}>{props.left}</div>
         <div css={{
           padding: '10px',
-          display: 'inline-block',
           width: '100%',
-          height: '20vh',
-          '@media (min-width: 576px)': {
-            height: 'calc(50vh - 42px)',
-          },
+          height: '50%',
           '@media (min-width: 992px)': {
             width: '50%',
-            height: 'calc(100vh - 84px)',
+            height: '100%',
           },
         }}>{props.right}</div>
       </div>
@@ -77,11 +76,11 @@ export const SquareAndCols = (props: SquareAndColsProps) => {
 export const SquareTest = (_: RouteComponentProps) => {
   return (
     <Page>
-    <SquareAndCols
-    square={<div css={{backgroundColor: 'blue', height: '100%'}}>a</div>}
-    left={<div css={{backgroundColor: 'green', height: '100%'}}>b</div>}
-    right={<div css={{backgroundColor: 'yellow', height: '100%'}}>c</div>}
-    />
+      <SquareAndCols
+        square={<div css={{ backgroundColor: 'blue', height: '100%' }}>a</div>}
+        left={<div css={{ backgroundColor: 'green', height: '100%' }}>b</div>}
+        right={<div css={{ backgroundColor: 'yellow', height: '100%' }}>c</div>}
+      />
     </Page>
   );
 }
