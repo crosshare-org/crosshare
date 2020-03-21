9ea16d530b6d108dc933923b6dd9167bd2571353
Michael Dirolf <mike@dirolf.com>
2020-03-21 14:03:36 -0400

    Add makeAdmin script in scripts sub-project

---
+++ crossword/scripts/src/makeAdmin.ts
@@ -0,0 +1,30 @@
+#!/usr/bin/env ts-node
+
+import * as admin from 'firebase-admin';
+
+import serviceAccount from '../../serviceAccountKey.json';
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+});
+
+async function grantAdminRole(userEmail: string): Promise<void> {
+  const user = await admin.auth().getUserByEmail(userEmail);
+  if (user.customClaims && (user.customClaims as any).admin === true) {
+    return;
+  }
+  return admin.auth().setCustomUserClaims(user.uid, {
+    admin: true,
+  });
+}
+
+if (process.argv.length !== 3) {
+  throw Error('Invalid use of makeAdmin. Usage: node makeAdmin.js <email>');
+}
+const email = process.argv[2];
+grantAdminRole(email).then(() => {
+  console.log(`User ${email} has been given admin role`);
+  process.exit(0);
+}).catch((err) => {
+  console.log('Failed to grant user admin role: ' + err);
+  process.exit(1);
+});
