e92ab9046ab83f490e4dd6d97117a541757e5efe
Michael Dirolf <mike@dirolf.com>
2020-03-26 12:43:59 -0400

    Use MessageChannel to interrupt worker

--- crossword/src/autofill.worker.ts
+++ crossword/src/autofill.worker.ts
@@ -2,12 +2,63 @@ const ctx: Worker = self as any;
 
 console.log("LOADED WORKER");
 
-ctx.onmessage = (e) => {
-  console.log("here");
-  const input = e.data.grid as Array<string>;
-  const grid = input.map((cell) => cell.trim() !== "" ? cell : String.fromCharCode(65+Math.floor(Math.random() * 26)));
+const msgChannel = new MessageChannel();
+let current: Autofiller;
 
-  console.log('Worker: Posting message back to main script');
-  ctx.postMessage({input, grid});
+msgChannel.port2.onmessage = _e => {
+  if (current.completed) {
+    return;
+  }
+  current.step()
+  msgChannel.port1.postMessage('');
+}
+
+const memo:Record<string, string[]> = {}
+class Autofiller {
+  public currentIter: number;
+  public completed: boolean;
+  public stringified: string;
+
+  constructor(public readonly grid: string[]) {
+    this.stringified = this.grid.join('|');
+    this.currentIter = 0;
+    this.completed = false;
+  }
+  solution() {
+    if (this.currentIter === 0) {
+      if (memo[this.stringified]) {
+        return memo[this.stringified];
+      }
+    }
+    if (this.currentIter === 20) {
+      const solution = this.grid.map((cell) => cell.trim() !== "" ? cell : String.fromCharCode(65+Math.floor(Math.random() * 26)));
+      memo[this.stringified] = solution;
+      return solution;
+    }
+    return null;
+  }
+  step() {
+    const solution = this.solution();
+    if (solution) {
+      console.log('Worker: Posting message back to main script');
+      ctx.postMessage({input: this.grid, grid: solution});
+      this.completed = true;
+      return;
+    }
+
+    var start = Date.now(),
+    now = start;
+    while (now - start < 100) {
+      now = Date.now();
+    }
+    this.currentIter += 1;
+  }
+}
+
+ctx.onmessage = (e) => {
+  console.log("Worker: got message");
+  const input = e.data.grid as Array<string>;
+  current = new Autofiller(input);
+  msgChannel.port1.postMessage('');
 }
 export default null as any;
