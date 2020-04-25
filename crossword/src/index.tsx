2feee9d43be6743cfc70e5b604e4b01def80d317
Michael Dirolf <mike@dirolf.com>
2020-04-25 09:36:34 -0400

    Don't log handled exceptions

--- crossword/src/index.tsx
+++ crossword/src/index.tsx
@@ -7,6 +7,12 @@ import * as Sentry from '@sentry/browser';
 
 if (process.env.NODE_ENV === 'production' && process.env.REACT_APP_SENTRY_RELEASE) {
   Sentry.init({
+    beforeSend(event) {
+      if (event ?.exception ?.values ?.[0].mechanism ?.handled) {
+        return null;
+      }
+      return event;
+    },
     dsn: "https://aef749dfcec64668bf922b8fbe4c0b41@o117398.ingest.sentry.io/5192748",
     release: process.env.REACT_APP_SENTRY_RELEASE,
   });
@@ -16,7 +22,7 @@ declare var firebase: typeof import('firebase');
 
 fetch('/__/firebase/init.json').then(async response => {
   const res = await response.json();
-  firebase.initializeApp({...res, authDomain: "auth.crosshare.org"});
+  firebase.initializeApp({ ...res, authDomain: "auth.crosshare.org" });
   if (process.env.NODE_ENV === 'production') {
     firebase.performance();
   }
