2edb80213ab0c14d5e53180964081a0ef48a7b56
Michael Dirolf <mike@dirolf.com>
2020-05-15 12:58:55 -0400

    hsl tests

--- crossword/src/utils.test.ts
+++ crossword/src/utils.test.ts
@@ -1,6 +1,6 @@
 import cases from 'jest-in-case';
 
-import { fnv1a } from './utils';
+import { fnv1a, hslToRgb } from './utils';
 
 
 cases('fnv1a hash function', opts => {
@@ -19,5 +19,16 @@ cases('fnv1a hash function', opts => {
     { name: 'foob', hash: 0x3f5076ef },
     { name: 'fooba', hash: 0x39aaa18a },
     { name: 'foobar', hash: 0xbf9cf968 },
+  ]
+);
+
+cases('hslToRgb color conversion', opts => {
+  expect(hslToRgb(opts.h, opts.s, opts.l)).toEqual(opts.rgb)
+},
+  [
+    { name: 'a', h: 0, s: 0, l: 0, rgb: [0, 0, 0] },
+    { name: 'b', h: 0.25, s: 0.56, l: 0.17, rgb: [43, 68, 19] },
+    { name: 'c', h: 0.561, s: 0.19, l: 0.39, rgb: [81, 105, 118] },
+    { name: 'd', h: 0.2, s: 0.55, l: 0.26, rgb: [88, 103, 30] },
   ]
 );
