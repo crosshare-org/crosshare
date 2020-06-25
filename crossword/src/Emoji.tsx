fb761b1c5a2d465518d0fcf81102f6fbd471dd2d
Michael Dirolf <mike@dirolf.com>
2020-06-25 13:58:49 -0400

    Remove old crossword/ create-react-app

--- crossword/src/Emoji.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-
-export const Emoji = (props: { title?: string, symbol: React.ReactNode }) => (
-  <span title={props.title} role='img' aria-label={props.title || 'emoji'}>{props.symbol}</span>
-);
