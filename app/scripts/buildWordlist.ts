1951429accac0a12da0cde42ea2ff8fc60c39f4c
Michael Dirolf <mike@dirolf.com>
2020-06-30 12:48:35 -0400

    Integrate cluer words into word db and add instructions to readme

--- app/scripts/buildWordlist.ts
+++ app/scripts/buildWordlist.ts
@@ -14,7 +14,7 @@ if (process.argv.length !== 5) {
 }
 
 const peters = process.argv[2];
-// const cluedata = process.argv[3];
+const cluedata = process.argv[3];
 const out = process.argv[4];
 
 const wordlist: Record<string, 0 | 1 | 2 | 3> = {};
@@ -32,22 +32,50 @@ const peterScoreMap = (petersScore: number): 0 | 1 | 2 | 3 => {
   return 3;
 };
 
-readFile(peters).then((contents: string) => {
-  const wordLines = String(contents).match(/[^\r\n]+/g);
+const cluedataScoreMap = (score: number): 0 | 1 | 2 | 3 => {
+  if (score < 5) {
+    return 0;
+  }
+  if (score < 15) {
+    return 1;
+  }
+  return 2;
+};
+
+function fileContentsToWords(contents: string): Array<[string, number]> {
+  const wordLines = contents.match(/[^\r\n]+/g);
   if (!wordLines) {
     throw new Error('malformed wordlist');
   }
-  const words: Array<[string, number]> = wordLines
+  return wordLines
     .map(s => s.toUpperCase().split(';'))
     .filter(s => !/[^A-Z]/.test(s[0])) /* Filter any words w/ non-letters */
-    .map((s): [string, number] => [s[0], parseInt(s[1])])
-    .filter(s => s[1] > 35); /* Filter out anything peter says he wouldnt use */
-  words.forEach(([word, score]) => {
-    wordlist[word] = peterScoreMap(score);
+    .map((s): [string, number] => [s[0], parseInt(s[1])]);
+}
+
+readFile(peters, 'utf8').then((pc: string) => {
+  const peterWords = fileContentsToWords(pc);
+  peterWords.forEach(([word, score]) => {
+    if (score >= 35) {
+      wordlist[word] = peterScoreMap(score);
+    }
   });
 
-  const outContent = Object.keys(wordlist).map(word => word + ';' + wordlist[word]).join('\n');
-  writeFile(out, outContent).then(() => {
-    console.log('wrote result to ' + out);
+  readFile(cluedata, 'utf8').then((cc: string) => {
+    const cluedataWords = fileContentsToWords(cc);
+    cluedataWords.forEach(([word, score]) => {
+      if (!wordlist[word]) {
+        const newScore = cluedataScoreMap(score);
+        if (newScore > 1) {
+          console.log('Adding ' + word);
+        }
+        wordlist[word] = newScore;
+      }
+    });
+
+    const outContent = Object.keys(wordlist).map(word => word + ';' + wordlist[word]).join('\n');
+    writeFile(out, outContent).then(() => {
+      console.log('wrote result to ' + out);
+    });
   });
 });
