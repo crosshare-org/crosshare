6cbbbb56a0b0513825e37915ba4231f58b7b21bb
Michael Dirolf <mike@dirolf.com>
2020-07-07 18:01:11 -0400

    Some cleanups for upload page

--- app/pages/upload.tsx
+++ app/pages/upload.tsx
@@ -5,15 +5,16 @@ import NextJSRouter from 'next/router';
 import { importFile } from '../lib/converter';
 import { requiresAuth } from '../components/AuthContext';
 import { STORAGE_KEY } from '../components/Builder';
+import { DefaultTopBar } from '../components/TopBar';
 
 export const UploadPage = () => {
-  const [warning, setWarning] = useState<string | null>(null);
+  const [warnExisting, setWarnExisting] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const inStorage = localStorage.getItem(STORAGE_KEY);
     if (inStorage) {
-      setWarning('WARNING: you have an existing puzzle in progress at crosshare.org/construct - uploading will cause you to permanently lose all progress. Please finish and publish the current puzzle before uploading.');
+      setWarnExisting(true);
     }
   }, []);
 
@@ -33,9 +34,10 @@ export const UploadPage = () => {
           const puzzle = importFile(new Uint8Array(fileReader.result));
           if (!puzzle) {
             setError('Failed to parse file');
+          } else {
+            localStorage.setItem(STORAGE_KEY, JSON.stringify(puzzle));
+            NextJSRouter.push('/construct');
           }
-          localStorage.setItem(STORAGE_KEY, JSON.stringify(puzzle));
-          NextJSRouter.push('/construct');
         } catch (error) {
           console.error(error);
           setError(error.message || 'Could not import file');
@@ -53,9 +55,19 @@ export const UploadPage = () => {
       <meta key="description" name="description" content={description} />
       <meta key="og:description" property="og:description" content={description} />
     </Head>
-    {warning ? <p css={{ color: 'var(--error)' }}>{warning}</p> : ''}
-    {error ? <p css={{ color: 'var(--error)' }}>{error}</p> : ''}
-    <input type='file' accept='.puz' onChange={e => handleFile(e.target.files)} />
+    <DefaultTopBar />
+    <div css={{ margin: '1em' }}>
+      {warnExisting ?
+        <p><span css={{ color: 'var(--error)' }}>WARNING:</span> You have an existing puzzle in progress at crosshare.org/construct - uploading will cause you to permanently lose all progress. Please finish and publish the current puzzle before uploading.</p>
+        : ''}
+      {error ?
+        <>
+          <p css={{ color: 'var(--error)' }}>{error}</p>
+          <p>If your puzzle isn&apos;t uploading correctly please message us on twitter or in the google group so we can help!</p>
+        </>
+        : ''}
+      <input type='file' accept='.puz' onChange={e => handleFile(e.target.files)} />
+    </div>
   </>;
 };
 
