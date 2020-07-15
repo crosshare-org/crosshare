ba5f84297eaa292391b29db0d3c2725ec92f75d8
Michael Dirolf <mike@dirolf.com>
2020-07-15 17:27:14 -0400

    Migration script to use .ca for .p #120

--- app/scripts/migrate.ts
+++ app/scripts/migrate.ts
@@ -1,35 +1,37 @@
-#!/usr/bin/env ts-node-script
+#!/usr/bin/env ts-node-script --skip-project -O {"resolveJsonModule":true}
 
 import * as admin from 'firebase-admin';
 
-/*import { PathReporter } from "io-ts/lib/PathReporter";
-import { isRight } from "fp-ts/lib/Either";
-import { PlayV, UserPlayT } from '../../functions/src/common/dbtypes';*/
+import { PathReporter } from 'io-ts/lib/PathReporter';
+import { isRight } from 'fp-ts/lib/Either';
+import { DBPuzzleV } from '../lib/dbtypes';
 
-import serviceAccount from '../../serviceAccountKey.json';
+import * as serviceAccount from '../../serviceAccountKey.json';
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
 
 if (process.argv.length !== 2) {
-  throw Error('Invalid use of makeAdmin. Usage: node migrate.js');
+  throw Error('Invalid use of migrate. Usage: ./scripts/migrate.ts');
 }
 
-//const db = admin.firestore()
+const db = admin.firestore();
 
 async function runMigration() {
-  /*  const value = await db.collection('p').get();
-    for (const doc of value.docs) {
-      const res = PlayV.decode(doc.data())
-      if (!isRight(res)) {
-        console.error(PathReporter.report(res).join(","));
-        throw new Error("Malformed play");
-      }
-      const userPlay: UserPlayT = [res.right.ua, res.right.t, res.right.ch, res.right.f, res.right.n];
-      db.collection('up').doc(res.right.u).set({
-        [res.right.c]: userPlay
-      }, { merge: true });
-    }*/
+  const value = await db.collection('c').get();
+  for (const doc of value.docs) {
+    const res = DBPuzzleV.decode(doc.data());
+    if (!isRight(res)) {
+      console.error(PathReporter.report(res).join(','));
+      console.error('Malformed puzzle: ' + doc.id);
+      continue;
+    }
+    const puzzle = res.right;
+    if (puzzle.ca.toMillis() !== puzzle.p ?.toMillis()) {
+      console.log('Updating puzzle ', doc.id);
+      await db.collection('c').doc(doc.id).update({ p: admin.firestore.Timestamp.fromMillis(puzzle.ca.toMillis()) });
+    }
+  }
 }
 
 runMigration().then(() => {
