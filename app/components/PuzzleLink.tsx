13ac3f830853a47e3fcd412dd9875ed8c8307f34
Michael Dirolf <mike@dirolf.com>
2020-07-23 09:03:39 -0400

    Style authored puzzle links specially #139

--- app/components/PuzzleLink.tsx
+++ app/components/PuzzleLink.tsx
@@ -8,18 +8,33 @@ import { SMALL_AND_UP } from '../lib/style';
 import { PuzzleSizeIcon } from '../components/Icons';
 import { Emoji } from '../components/Emoji';
 import { timeString } from '../lib/utils';
+import { getFromSessionOrDB } from '../lib/dbUtils';
+import { AuthoredPuzzlesV } from '../lib/dbtypes';
 
 export const PuzzleLink = (props: { id: string, width?: number, height?: number, title: string, byline?: ReactNode, children?: ReactNode }) => {
   const { user } = useContext(AuthContext);
   const [plays, setPlays] = useState<PlayMapT | null>(null);
+  const [authored, setAuthored] = useState(false);
 
   useEffect(() => {
+    if (user) {
+      getFromSessionOrDB({ collection: 'uc', docId: user.uid, validator: AuthoredPuzzlesV, ttl: 5 * 60 * 1000 })
+        .then(aps => {
+          if (aps !== null) {
+            setAuthored(Object.prototype.hasOwnProperty.call(aps, props.id));
+          }
+        })
+        .catch(reason => {
+          console.error(reason);
+        });
+    }
+
     getPlays(user)
       .then(pm => setPlays(pm))
       .catch(reason => {
         console.error(reason);
       });
-  }, [user]);
+  }, [user, props.id]);
 
   const play = plays && plays[props.id];
 
@@ -32,22 +47,26 @@ export const PuzzleLink = (props: { id: string, width?: number, height?: number,
     },
   }}>
     <Link css={{
-      color: (play && play.f) ? 'var(--text)' : (play ? 'var(--error)' : 'var(--link)'),
+      color: (authored || (play && play.f)) ? 'var(--text)' : (play ? 'var(--error)' : 'var(--link)'),
       '&:hover': {
-        color: (play && play.f) ? 'var(--text)' : (play ? 'var(--error-hover)' : 'var(--link-hover)'),
+        color: (authored || (play && play.f)) ? 'var(--text)' : (play ? 'var(--error-hover)' : 'var(--link-hover)'),
       }
     }} href='/crosswords/[puzzleId]' as={`/crosswords/${props.id}`} passHref>
       <div css={{ position: 'relative', verticalAlign: 'top !important', float: 'left', fontSize: '4em', marginRight: '0.3em' }} >
         <PuzzleSizeIcon width={props.width} height={props.height} />
-        {play && play.f ?
+        {authored || (play && play.f) ?
           <div css={{ textShadow: '2px 0 0 white, -2px 0 0 white, 0 2px 0 white, 0 -2px 0 white', position: 'absolute', top: '0.1em', left: '0.35em', fontSize: '0.6em' }}>
-            {play.ch ? <Emoji title='Used helpers' symbol='😏' /> : <Emoji title='Solved without helpers' symbol='🤓' />}
+            {authored ?
+              <Emoji title='Authored Puzzle' symbol='🖋️' />
+              :
+              (play && play.ch ? <Emoji title='Used helpers' symbol='😏' /> : <Emoji title='Solved without helpers' symbol='🤓' />)
+            }
           </div>
           :
           ''
         }
       </div>
-      <h3>{props.title} {play ? (play.f ? <i>({timeString(play.t, false)})</i> : <i>(unfinished)</i>) : ''}</h3>
+      <h3>{props.title} {!authored && play ? (play.f ? <i>({timeString(play.t, false)})</i> : <i>(unfinished)</i>) : ''}</h3>
       {props.byline}
     </Link>
     {props.children}
