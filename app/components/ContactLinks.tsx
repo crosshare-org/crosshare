88e50c55e11880e5491328eb3064ce2b5158e2bf
Mike Dirolf <mike@dirolf.com>
2020-12-18 15:33:27 -0500

    Forgot to commit this

---
+++ app/components/ContactLinks.tsx
@@ -0,0 +1,21 @@
+export function ContactLinks() {
+  return (
+    <>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="mailto:crosshareapp@gmail.com"
+      >
+        email
+      </a>{' '}
+      or{' '}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://twitter.com/crosshareapp"
+      >
+        twitter
+      </a>
+    </>
+  );
+}
