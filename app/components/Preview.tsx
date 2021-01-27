75bb31ed6902e05f6ea6dddd94e6e4c38e12f9ea
Mike Dirolf <mike@dirolf.com>
2021-01-27 14:27:44 -0500

    Show publish errors on upload too

--- app/components/Preview.tsx
+++ app/components/Preview.tsx
@@ -162,6 +162,24 @@ export const Preview = (props: PuzzleInProgressT & AuthProps): JSX.Element => {
         ) : (
           ''
         )}
+        {state.publishErrors.length ? (
+          <Overlay
+            closeCallback={() => dispatch({ type: 'CLEARPUBLISHERRORS' })}
+          >
+            <>
+              <div>
+                Please fix the following errors and try publishing again:
+              </div>
+              <ul>
+                {state.publishErrors.map((s, i) => (
+                  <li key={i}>{s}</li>
+                ))}
+              </ul>
+            </>
+          </Overlay>
+        ) : (
+          ''
+        )}
         {!dismissedIntro ? (
           <Overlay closeCallback={() => setDismissedIntro(true)}>
             <h2>
