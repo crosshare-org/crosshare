911f9df47a5dd78b2151902ac0362ecefe23797f
Mike Dirolf <mike@dirolf.com>
2021-04-02 12:23:23 -0400

    Move firebase config to its own file for easier local development

--- app/lib/firebaseWrapper.ts
+++ app/lib/firebaseWrapper.ts
@@ -5,19 +5,9 @@ import 'firebase/firestore';
 import 'firebase/performance';
 import 'firebase/auth';
 import 'firebase/storage';
+import { firebaseConfig } from '../firebaseConfig';
 
 import type firebaseAdminType from 'firebase-admin';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyBrmmBf91peVT5T_Z7N3z9oizsPH5u2pUc',
-  authDomain: 'auth.crosshare.org',
-  databaseURL: 'https://mdcrosshare.firebaseio.com',
-  projectId: 'mdcrosshare',
-  storageBucket: 'mdcrosshare.appspot.com',
-  messagingSenderId: '603173482014',
-  appId: '1:603173482014:web:98d7d820731b7c5eaa080f',
-  measurementId: 'G-LTLN7Z4XBS',
-};
 
 // Initialize Firebase
 export let App: firebase.app.App;
