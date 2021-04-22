b210dc28573ed21a64abfa895e59008eb779864a
Mike Dirolf <mike@dirolf.com>
2021-04-22 10:01:35 -0400

    Script to transform accounts.csv

---
+++ app/scripts/transformAccountsCSV.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env -S npx ts-node-script
+
+import fs from 'fs';
+import util from 'util';
+
+import parse from 'csv-parse/lib/sync';
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+readFile('accounts.csv').then((binary) => {
+  const csv: Array<Array<string>> = parse(binary, {
+    quote: null,
+    escape: null,
+  });
+  const out = csv
+    .filter((r) => r[1])
+    .map((r) => r[1])
+    .join('\n');
+  writeFile('accountsTransformed.csv', out).then(() => {
+    console.log('done');
+  });
+});
