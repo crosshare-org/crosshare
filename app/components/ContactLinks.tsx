ff5592963daa89017c1989b3af3ca7510448e482
Mike Dirolf <mike@dirolf.com>
2021-11-11 10:22:53 -0500

    Add discord link

--- app/components/ContactLinks.tsx
+++ app/components/ContactLinks.tsx
@@ -3,21 +3,28 @@ import { Trans } from '@lingui/macro';
 export function ContactLinks() {
   return (
     <>
-      <Trans>
+      <Trans id="contact">
         <a
           target="_blank"
           rel="noopener noreferrer"
           href="mailto:mike@crosshare.org"
         >
           email
-        </a>{' '}
-        or{' '}
+        </a>,{' '}
         <a
           target="_blank"
           rel="noopener noreferrer"
           href="https://twitter.com/crosshareapp"
         >
           twitter
+        </a>,
+        or{' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://discord.gg/8Tu67jB4F3"
+        >
+          discord
         </a>
       </Trans>
     </>
