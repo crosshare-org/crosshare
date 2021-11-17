1c85019046cb1459f8a55a380c0ddbab1730da89
Mike Dirolf <mike@dirolf.com>
2021-11-17 11:09:58 -0500

    Just check for invalid puzzles

--- app/scripts/migrate.ts
+++ app/scripts/migrate.ts
@@ -1,16 +1,27 @@
 #!/usr/bin/env -S npx ts-node-script
 
-// import { AdminApp } from '../lib/firebaseWrapper';
+import { isRight } from 'fp-ts/lib/Either';
+import { DBPuzzleV } from '../lib/dbtypes';
+import { AdminApp } from '../lib/firebaseWrapper';
 export {};
 
 if (process.argv.length !== 2) {
   throw Error('Invalid use of migrate. Usage: ./scripts/migrate.ts');
 }
 
-// const db = AdminApp.firestore();
+const db = AdminApp.firestore();
 
 async function runMigration() {
   console.log('Run migration here...');
+  return db.collection('c').get().then(snap => {
+    snap.forEach(async doc => {
+      const validationResult = DBPuzzleV.decode(doc.data());
+      if (!isRight(validationResult)) {
+        console.log('INVALID', doc.id);
+        return;
+      }
+    });
+  });
 }
 
 runMigration().then(() => {
