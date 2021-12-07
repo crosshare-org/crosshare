a1c5a340cf57f376608ede269ffde056b42edafa
Mike Dirolf <mike@dirolf.com>
2021-12-07 15:30:57 -0500

    Forgot to commit this

--- app/lib/gtag.ts
+++ app/lib/gtag.ts
@@ -9,11 +9,24 @@ export const pageview = (url: string) => {
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export const event = ({ action, category, label, value }: { action: string, category: string, label: string, value?: number }) => {
+export const event = ({
+  action,
+  category,
+  label,
+  value,
+  nonInteraction,
+}: {
+  action: string;
+  category: string;
+  label: string;
+  value?: number;
+  nonInteraction?: boolean;
+}) => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (window as any).gtag('event', action, {
     event_category: category,
     event_label: label,
     value: value,
+    ...(nonInteraction && { non_interaction: true }),
   });
 };
