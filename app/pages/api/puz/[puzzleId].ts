19f22b933021d077d840ae54cd92f39e41056a11
Mike Dirolf <mike@dirolf.com>
2021-12-12 16:03:45 -0500

    Undo changes to api/puz/...

--- app/pages/api/puz/[puzzleId].ts
+++ app/pages/api/puz/[puzzleId].ts
@@ -3,15 +3,11 @@ import { exportFile } from '../../../lib/converter';
 import { getPuzzle } from '../../../lib/puzzleCache';
 
 export default async function puz(req: NextApiRequest, res: NextApiResponse) {
-  const { urlTokens } = req.query;
-  const [ puzzleId ] = urlTokens || '';
-  console.log('here we are');
-  console.log(req.query);
-  if (!puzzleId) {
-    console.log('testing');
+  const { puzzleId } = req.query;
+  if (!puzzleId || Array.isArray(puzzleId)) {
     return res
       .status(404)
-      .json({ statusCode: 404, message: 'bad puzzle params', params: req.query });
+      .json({ statusCode: 404, message: 'bad puzzle params' });
   }
   const puzzle = await getPuzzle(puzzleId);
   if (!puzzle) {
