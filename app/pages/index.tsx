d58d682b0ff15ccad2b2b50ba199255a4159013d
Mike Dirolf <mike@dirolf.com>
2021-12-15 09:50:01 -0500

    Add donation campaign to homepage

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -27,6 +27,7 @@ import { useRouter } from 'next/router';
 import { paginatedPuzzles } from '../lib/paginatedPuzzles';
 import { isUserPatron } from '../lib/patron';
 import { isSome } from 'fp-ts/lib/Option';
+import { PatronIcon } from '../components/Icons';
 
 type HomepagePuz = LinkablePuzzle & {
   constructorPage: ConstructorPageT | null;
@@ -119,6 +120,39 @@ export default function HomePage({
       <DefaultTopBar />
 
       <div css={{ margin: '1em' }}>
+        <Link
+          css={{
+            display: 'block',
+            textDecoration: 'none',
+            color: 'var(--text)',
+            border: '1px solid var(--error)',
+            borderRadius: '0.5em',
+            padding: '1em',
+            marginBottom: '1em',
+            '&:hover': {
+              color: 'var(--text)',
+              textDecoration: 'none',
+            },
+          }}
+          href="/donate"
+        >
+          <h3>
+            <span css={{ color: 'var(--error)' }}>Read this</span> - we need
+            your help!
+          </h3>
+          <div>
+            As Crosshare continues to grow (and add new features) I need help to
+            pay for the ongoing costs of running the site. This holiday season /
+            new year, I&apos;m hoping to reach $100/month in new recurring
+            donations to keep the site going through 2022 and beyond. Please
+            consider contributing whatever you are able - think of it like a
+            Netflix subscription, but instead of helping a huge faceless
+            multinational provide mindless entertainment you&apos;re helping a
+            volunteer team of one provide mindful entertainment 😂. All monthly
+            contributers get a new patron icon - <PatronIcon /> - so we all know
+            who to thank for making the site possible!
+          </div>
+        </Link>
         <p css={{ marginBottom: '1em' }}>
           <Trans id="crosshare-intro">
             Crosshare is a <b>free</b>, <b>ad-free</b>, and{' '}
