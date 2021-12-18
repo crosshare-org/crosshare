ee5f4ce082e49d334628c950de95aef2b20ffd16
Mike Dirolf <mike@dirolf.com>
2021-12-18 15:23:21 -0500

    Basic test for entriesFromCells w/ and w/o barred grid

--- app/__tests__/grid.test.ts
+++ app/__tests__/grid.test.ts
@@ -1,3 +1,4 @@
+import { entriesFromCells } from '../lib/gridBase';
 import {
   addClues,
   CluedGrid,
@@ -5,6 +6,221 @@ import {
   getEntryToClueMap,
   getRefs,
 } from '../lib/viewableGrid';
+
+test('test entriesFromCells', () => {
+  expect(entriesFromCells(2, 2, ['A', 'B', 'C', 'D'], new Set(), new Set()))
+    .toMatchInlineSnapshot(`
+Array [
+  Array [
+    Object {
+      "cells": Array [
+        Object {
+          "col": 0,
+          "row": 0,
+        },
+        Object {
+          "col": 1,
+          "row": 0,
+        },
+      ],
+      "completedWord": "AB",
+      "direction": 0,
+      "index": 0,
+      "pattern": "AB",
+    },
+    Object {
+      "cells": Array [
+        Object {
+          "col": 0,
+          "row": 0,
+        },
+        Object {
+          "col": 0,
+          "row": 1,
+        },
+      ],
+      "completedWord": "AC",
+      "direction": 1,
+      "index": 1,
+      "pattern": "AC",
+    },
+    Object {
+      "cells": Array [
+        Object {
+          "col": 1,
+          "row": 0,
+        },
+        Object {
+          "col": 1,
+          "row": 1,
+        },
+      ],
+      "completedWord": "BD",
+      "direction": 1,
+      "index": 2,
+      "pattern": "BD",
+    },
+    Object {
+      "cells": Array [
+        Object {
+          "col": 0,
+          "row": 1,
+        },
+        Object {
+          "col": 1,
+          "row": 1,
+        },
+      ],
+      "completedWord": "CD",
+      "direction": 0,
+      "index": 3,
+      "pattern": "CD",
+    },
+  ],
+  Array [
+    Array [
+      Object {
+        "cellIndex": 0,
+        "entryIndex": 0,
+        "wordIndex": 0,
+      },
+      Object {
+        "cellIndex": 0,
+        "entryIndex": 1,
+        "wordIndex": 0,
+      },
+    ],
+    Array [
+      Object {
+        "cellIndex": 1,
+        "entryIndex": 0,
+        "wordIndex": 1,
+      },
+      Object {
+        "cellIndex": 0,
+        "entryIndex": 2,
+        "wordIndex": 0,
+      },
+    ],
+    Array [
+      Object {
+        "cellIndex": 0,
+        "entryIndex": 3,
+        "wordIndex": 0,
+      },
+      Object {
+        "cellIndex": 1,
+        "entryIndex": 1,
+        "wordIndex": 1,
+      },
+    ],
+    Array [
+      Object {
+        "cellIndex": 1,
+        "entryIndex": 3,
+        "wordIndex": 1,
+      },
+      Object {
+        "cellIndex": 1,
+        "entryIndex": 2,
+        "wordIndex": 1,
+      },
+    ],
+  ],
+]
+`);
+
+  expect(
+entriesFromCells(2, 2, ['A', 'B', 'C', 'D'], new Set([0]), new Set([1]))).
+toMatchInlineSnapshot(`
+Array [
+  Array [
+    Object {
+      "cells": Array [
+        Object {
+          "col": 0,
+          "row": 0,
+        },
+        Object {
+          "col": 0,
+          "row": 1,
+        },
+      ],
+      "completedWord": "AC",
+      "direction": 1,
+      "index": 0,
+      "pattern": "AC",
+    },
+    Object {
+      "cells": Array [
+        Object {
+          "col": 0,
+          "row": 1,
+        },
+        Object {
+          "col": 1,
+          "row": 1,
+        },
+      ],
+      "completedWord": "CD",
+      "direction": 0,
+      "index": 1,
+      "pattern": "CD",
+    },
+  ],
+  Array [
+    Array [
+      Object {
+        "cellIndex": 0,
+        "entryIndex": null,
+        "wordIndex": 0,
+      },
+      Object {
+        "cellIndex": 0,
+        "entryIndex": 0,
+        "wordIndex": 0,
+      },
+    ],
+    Array [
+      Object {
+        "cellIndex": 0,
+        "entryIndex": null,
+        "wordIndex": 0,
+      },
+      Object {
+        "cellIndex": 0,
+        "entryIndex": null,
+        "wordIndex": 0,
+      },
+    ],
+    Array [
+      Object {
+        "cellIndex": 0,
+        "entryIndex": 1,
+        "wordIndex": 0,
+      },
+      Object {
+        "cellIndex": 1,
+        "entryIndex": 0,
+        "wordIndex": 1,
+      },
+    ],
+    Array [
+      Object {
+        "cellIndex": 1,
+        "entryIndex": 1,
+        "wordIndex": 1,
+      },
+      Object {
+        "cellIndex": 0,
+        "entryIndex": null,
+        "wordIndex": 0,
+      },
+    ],
+  ],
+]
+`);
+});
 
 test('test getEntryToClueMap', () => {
   const answers = ['U', 'P', 'S', 'O'];
