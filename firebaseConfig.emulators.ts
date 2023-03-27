9b3ba1a65af01c4fd5463680f92bf50cd1da9b86
Mike D <mike@dirolf.com>
2023-03-26 20:00:25 -0400

    Not sure this is necessary but it doesn't hurt

--- firebaseConfig.emulators.ts
+++ firebaseConfig.emulators.ts
@@ -2,4 +2,5 @@ export const firebaseConfig = {
   projectId: 'demo-crosshare',
   apiKey: 'test',
   authDomain: 'crosshare-devel.firebaseapp.com',
+  storageBucket: 'demo-crosshare.appspot.com',
 };
