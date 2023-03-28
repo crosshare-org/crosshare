0552ebd97fa08c35a3a556855599385914334a3a
Mike D <mike@dirolf.com>
2023-03-28 13:15:27 -0400

    A (slightly) more involved playwright test

--- app/e2e/example.spec.ts
+++ app/e2e/example.spec.ts
@@ -6,3 +6,12 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Crosshare/);
 });
+
+test('reveal puzzle', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('link', { name: 'Just like Jeff!' }).click();
+  await page.getByRole('button', { name: 'Begin Puzzle' }).click();
+  await page.getByRole('button', { name: 'Reveal' }).click();
+  await page.getByRole('button', { name: 'Reveal Puzzle' }).click();
+  await expect(page.getByText('Hey cool puzzle - I especially liked 17A')).toHaveCount(1);
+});
