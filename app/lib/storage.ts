d1a66ae0d7b98172eb701d45265019fd36b1b9fe
Mike D <mike@dirolf.com>
2024-04-11 12:32:40 -0400

    Forgot to commit new storage lib

---
+++ app/lib/storage.ts
@@ -0,0 +1,35 @@
+import { isRight } from 'fp-ts/lib/Either';
+import * as t from 'io-ts';
+import { PathReporter } from 'io-ts/lib/PathReporter';
+
+export function fromLocalStorage<A>(
+  key: string,
+  validator: t.Type<A>
+): A | null {
+  let inSession: string | null;
+  try {
+    inSession = localStorage.getItem(key);
+  } catch {
+    /* happens on incognito when iframed */
+    console.warn(`couldn't load ${key} from LS`);
+    inSession = null;
+  }
+  if (inSession) {
+    const res = validator.decode(JSON.parse(inSession));
+    if (isRight(res)) {
+      return res.right;
+    } else {
+      console.error("Couldn't parse object in local storage");
+      console.error(PathReporter.report(res).join(','));
+    }
+  }
+  return null;
+}
+
+export function arrayFromLocalStorage<A>(
+  key: string,
+  validator: t.Type<A>
+): A[] {
+  const res = fromLocalStorage(key, t.array(validator));
+  return res || [];
+}
