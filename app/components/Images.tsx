74e7199051730bd987d7d8c3b67aa6b9fd73ea30
Mike D <mike@dirolf.com>
2024-04-28 14:39:47 -0400

    Images emotion to css module and a few fixes

--- app/components/Images.tsx
+++ app/components/Images.tsx
@@ -1,34 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 import { ReactNode } from 'react';
-import { LARGE_AND_UP, SMALL_AND_UP } from '../lib/style';
+import { clsx } from '../lib/utils';
+import styles from './Images.module.css';
 
 const ProfilePic = (props: { profilePicture: string; className?: string }) => {
   return (
-    <div
-      className={props.className}
-      css={{
-        width: 75,
-        height: 75,
-        position: 'relative',
-        overflow: 'hidden',
-        [SMALL_AND_UP]: {
-          width: 100,
-          height: 100,
-        },
-        boxSizing: 'content-box',
-        border: '4px solid var(--bg)',
-        borderRadius: '50%',
-      }}
-    >
+    <div className={clsx(styles.profilePicWrapper, props.className)}>
       <img
-        css={{
-          width: 75,
-          height: 75,
-          [SMALL_AND_UP]: {
-            width: 100,
-            height: 100,
-          },
-        }}
+        className={styles.profilePicImage}
         width={200}
         height={200}
         src={props.profilePicture}
@@ -41,20 +20,11 @@ const ProfilePic = (props: { profilePicture: string; className?: string }) => {
 export const CoverPic = (props: { coverPicture: string }) => {
   return (
     <div
-      css={{
-        width: '100%',
-        height: '150px',
-        [SMALL_AND_UP]: {
-          height: '225px',
-        },
-        [LARGE_AND_UP]: {
-          height: '300px',
-        },
+      style={{
         backgroundImage: `linear-gradient(to top, black, rgba(0, 0, 0, 0.8) 2em, transparent 40%, transparent 75%, rgba(0, 0, 0, 0.5)),
-    url('${props.coverPicture}')`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
+      url('${props.coverPicture}')`,
       }}
+      className={styles.coverPic}
     />
   );
 };
@@ -68,42 +38,27 @@ export const ProfilePicAndName = (props: {
 }) => {
   return (
     <div
-      css={{
-        display: 'flex',
+      style={{
         ...(props.coverImage && {
           marginTop: `${-4.5 - (props.bonusMargin || 0)}em`,
         }),
       }}
+      className="displayFlex"
     >
       {props.profilePic ? (
-        <div css={{ flex: '1 1 auto', textAlign: 'right' }}>
+        <div className={styles.picWrap}>
           <ProfilePic
-            css={{
-              display: 'inline-block',
-              marginTop: '-0.5em',
-              marginRight: '1em',
-              [SMALL_AND_UP]: { marginTop: '-1.5em' },
-            }}
+            className={styles.pic}
             profilePicture={props.profilePic}
           />
         </div>
       ) : (
         ''
       )}
-      <div
-        css={{
-          flex: '1 1 auto',
-          textAlign: props.profilePic ? 'left' : 'center',
-        }}
-      >
+      <div data-has-pic={Boolean(props.profilePic)} className={styles.textArea}>
         <h1
-          css={{
-            fontSize: '1.5em',
-            ...(props.coverImage && {
-              color: 'white',
-              mixBlendMode: 'difference',
-            }),
-          }}
+          data-has-cover={Boolean(props.coverImage)}
+          className={styles.topLine}
         >
           {props.topLine}
         </h1>
