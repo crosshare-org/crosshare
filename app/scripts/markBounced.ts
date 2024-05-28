0bfc265947970b5b4f3da9e4777ab630aea22f39
Mike D <mike@dirolf.com>
2024-05-28 11:48:43 -0400

    markBounced accepts bulk list now

--- app/scripts/markBounced.ts
+++ app/scripts/markBounced.ts
@@ -4,32 +4,36 @@ import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getAdminApp } from '../lib/firebaseAdminWrapper.js';
 
-if (process.argv.length !== 3) {
+if (process.argv.length < 3) {
   throw Error(
-    'Invalid use of markBounced. Usage: ./scripts/markBounced.ts [email]'
+    'Invalid use of markBounced. Usage: ./scripts/markBounced.ts [...emails]'
   );
 }
 
 export async function markBounced() {
-  const emailToUnsub = process.argv[2]?.trim();
-  if (!emailToUnsub) {
-    console.error('missing email');
-    return;
+  for (const arg of process.argv.splice(2)) {
+    const emailToUnsub = arg.trim();
+    if (!emailToUnsub) {
+      console.error('missing email');
+      return;
+    }
+
+    console.log('unsubbing', arg);
+
+    const db = getFirestore(getAdminApp());
+
+    await getAuth()
+      .getUserByEmail(emailToUnsub)
+      .then((user) =>
+        db
+          .collection('prefs')
+          .doc(user.uid)
+          .set({ bounced: true }, { merge: true })
+      )
+      .catch((error: unknown) => {
+        console.error('error setting bounced', error);
+      });
   }
-
-  const db = getFirestore(getAdminApp());
-
-  await getAuth()
-    .getUserByEmail(emailToUnsub)
-    .then((user) =>
-      db
-        .collection('prefs')
-        .doc(user.uid)
-        .set({ bounced: true }, { merge: true })
-    )
-    .catch((error: unknown) => {
-      console.error('error setting bounced', error);
-    });
 }
 
 markBounced()
