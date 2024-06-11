0e51d3070e77f543e50e3403f88aea598032dde4
Mike D <mike@dirolf.com>
2024-06-11 16:31:31 -0400

    Just add some stubs for the other pack pages

---
+++ app/pages/packs/[packId]/edit.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import { DefaultTopBar } from '../../../components/TopBar.js';
+import { withTranslation } from '../../../lib/translation.js';
+
+export const getServerSideProps = withTranslation(() => {
+  return Promise.resolve({ props: {} });
+});
+
+export default function Page() {
+  return (
+    <>
+      <Head>
+        <title>{`Edit Pack | Crosshare Crossword Constructor and Puzzles`}</title>
+      </Head>
+      <DefaultTopBar />
+      <div className="margin1em">
+        <h2>Coming soon...</h2>
+      </div>
+    </>
+  );
+}
