8e3c9fec92a60df906b5330893c4286692173bee
Mike D <mike@dirolf.com>
2024-12-09 17:26:15 +0000

    Add script to edit donation records

---
+++ app/scripts/donations.ts
@@ -0,0 +1,67 @@
+#!/usr/bin/env -S npx tsx
+
+import { command, oneOf, positional, run, string, subcommands } from 'cmd-ts';
+import { DonationsListV } from '../lib/dbtypes.js';
+import { getCollection } from '../lib/firebaseAdminWrapper.js';
+import { PathReporter } from '../lib/pathReporter.js';
+
+const donationsCollection = getCollection('donations');
+
+const edit = command({
+  name: 'edit',
+  args: {
+    email: positional({ type: string, displayName: 'email' }),
+    field: positional({
+      type: oneOf(['username']),
+      displayName: 'field to update',
+    }),
+    value: positional({
+      type: string,
+      displayName: 'new value',
+    }),
+  },
+  handler: async (args) => {
+    const dbres = await donationsCollection.doc(`donations`).get();
+    if (!dbres.exists) {
+      console.error('no donations doc');
+      return;
+    }
+    const validationResult = DonationsListV.decode(dbres.data());
+    if (validationResult._tag !== 'Right') {
+      console.error(PathReporter.report(validationResult).join(','));
+      return;
+    }
+    const donations = validationResult.right;
+
+    if (args.field !== 'username') {
+      console.error('unimplemented!');
+      return;
+    }
+
+    let success = false;
+    for (let i = donations.d.length - 1; i >= 0; i -= 1) {
+      const o = donations.d[i];
+      if (o && o.e.trim().toLowerCase() === args.email.trim().toLowerCase()) {
+        o.p = args.value.trim();
+        console.log(o);
+        success = true;
+        break;
+      }
+    }
+
+    if (!success) {
+      console.error('email not found in donors list');
+      return;
+    }
+
+    await donationsCollection.doc(`donations`).set(donations);
+    console.log('updated');
+  },
+});
+
+const cmd = subcommands({
+  name: 'donations',
+  cmds: { edit },
+});
+
+void run(cmd, process.argv.slice(2));
