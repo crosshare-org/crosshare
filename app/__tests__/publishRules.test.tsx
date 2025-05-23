0f1aff460e84ba615f74b0b0be84507f1b9e7b04
Mike D <mike@dirolf.com>
2025-05-23 12:53:53 +0000

    Basic rules for styles

--- app/__tests__/publishRules.test.tsx
+++ app/__tests__/publishRules.test.tsx
@@ -85,6 +85,59 @@ test('security rules should not allow publishing with restricted fields set', as
   );
 });
 
+test('sanity check cell styles', async () => {
+  const firestore = testEnv
+    .authenticatedContext('fSEwJorvqOMK5UhNMHa4mu48izl1')
+    .firestore();
+
+  await assertSucceeds(
+    addDoc(collection(firestore, 'c'), {
+      ...puzzle,
+      sty: { circle: [1, 4], 'rgb(100, 20, 10)': [1] },
+    })
+  );
+
+  // duplicate
+  await assertFails(
+    addDoc(collection(firestore, 'c'), { ...puzzle, sty: { circle: [1, 1] } })
+  );
+
+  // too many styles
+  await assertFails(
+    addDoc(collection(firestore, 'c'), {
+      ...puzzle,
+      sty: {
+        circle: [1, 4],
+        'rgb(100, 20, 10)': [1],
+        'rgb(101, 20, 10)': [1],
+        'rgb(102, 20, 10)': [1],
+        'rgb(103, 20, 10)': [1],
+        'rgb(104, 20, 10)': [1],
+        'rgb(105, 20, 10)': [1],
+        'rgb(106, 20, 10)': [1],
+        'rgb(107, 20, 10)': [1],
+        'rgb(108, 20, 10)': [1],
+        'rgb(109, 20, 10)': [1],
+        'rgb(110, 20, 10)': [1],
+        'rgb(120, 20, 10)': [1],
+        'rgb(130, 20, 10)': [1],
+        'rgb(140, 20, 10)': [1],
+        'rgb(150, 20, 10)': [1],
+        'rgb(160, 20, 10)': [1],
+        'rgb(170, 20, 10)': [1],
+      },
+    })
+  );
+
+  // we can't check out of bounds, so just make sure list doesn't exceed puzzle size
+  await assertFails(
+    addDoc(collection(firestore, 'c'), {
+      ...puzzle,
+      sty: { circle: [...Array(30).keys()] },
+    })
+  );
+});
+
 test('security rules should not allow publishing if fake author-id', async () => {
   const firestore = testEnv.authenticatedContext('mike').firestore();
 
