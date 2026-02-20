ede33f4dfa07101824e7dcd283376a84266c6421
Mike D <mike@dirolf.com>
2026-02-20 19:19:15 +0000

    Move donationsForYear into donations script, add command to compact donations in DB

--- app/scripts/donations.ts
+++ app/scripts/donations.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S npx tsx
 
 import { command, oneOf, positional, run, string, subcommands } from 'cmd-ts';
-import { DonationsListV } from '../lib/dbtypes.js';
+import { DonationsListT, DonationsListV } from '../lib/dbtypes.js';
 import { getCollection } from '../lib/firebaseAdminWrapper.js';
 import { PathReporter } from '../lib/pathReporter.js';
 
@@ -42,6 +42,60 @@ const print = command({
       console.error('email not found in donors list');
       return;
     }
+  },
+});
+
+const compact = command({
+  name: 'compact',
+  args: {},
+  handler: async () => {
+    const dbres = await donationsCollection.doc(`donations`).get();
+    if (!dbres.exists) {
+      console.error('no donations doc');
+      return;
+    }
+    const validationResult = DonationsListV.decode(dbres.data());
+    if (validationResult._tag !== 'Right') {
+      console.error(PathReporter.report(validationResult).join(','));
+      return;
+    }
+    const donations: DonationsListT = validationResult.right;
+    await donationsCollection
+      .doc(`history-${new Date().toISOString()}`)
+      .set(donations);
+    console.log('pre-compact length:', donations.d.length);
+
+    // Reduce right so the newest entries are examined first. That way we use the most recent date.
+    const compacted: DonationsListT = donations.d.reduceRight(
+      (accumulator: DonationsListT, current) => {
+        let found = false;
+        for (const existing of accumulator.d) {
+          // Compact anything for the same email in the same year
+          if (existing.e !== current.e) continue;
+          if (
+            existing.d.toDate().getFullYear() !==
+            current.d.toDate().getFullYear()
+          )
+            continue;
+          found = true;
+          existing.a += current.a;
+          existing.r += current.r;
+          existing.n = existing.n || current.n;
+          existing.p = existing.p || current.p;
+          if (current.u && !existing.u) {
+            existing.u = current.u;
+          }
+        }
+        if (!found) {
+          accumulator.d.push(current);
+        }
+        return accumulator;
+      },
+      { d: [] }
+    );
+    compacted.d.reverse();
+    console.log('post-compact length:', compacted.d.length);
+    await donationsCollection.doc(`donations`).set(compacted);
   },
 });
 
@@ -101,9 +155,37 @@ const edit = command({
   },
 });
 
+const byYear = command({
+  name: 'byYear',
+  args: {},
+  handler: async () => {
+    const dbres = await donationsCollection.doc(`donations`).get();
+    if (!dbres.exists) {
+      console.error('no donations doc');
+      return;
+    }
+    const validationResult = DonationsListV.decode(dbres.data());
+    if (validationResult._tag !== 'Right') {
+      console.error(PathReporter.report(validationResult).join(','));
+      return;
+    }
+    const donations: DonationsListT = validationResult.right;
+    const byYear: Record<number, number> = {};
+    for (const donation of donations.d) {
+      const year = donation.d.toDate().getFullYear();
+      if (byYear[year]) {
+        byYear[year] += donation.r;
+      } else {
+        byYear[year] = donation.r;
+      }
+    }
+    console.log(byYear);
+  },
+});
+
 const cmd = subcommands({
   name: 'donations',
-  cmds: { edit, print },
+  cmds: { edit, print, compact, byYear },
 });
 
 void run(cmd, process.argv.slice(2));
