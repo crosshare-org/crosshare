66baa0c37bf66456f593ba7a13bfc63636bf494d
Kailash Nelson <37966146+KingLizard1020@users.noreply.github.com>
2026-09-04 12:23:35 -0600

    Add test for notification title updates when a puzzle is renamed

--- app/__tests__/puzzleUpdate.test.ts
+++ app/__tests__/puzzleUpdate.test.ts
@@ -1,1 +1,29 @@
-FULL_FILE_FROM_JSON
+/**
+ * @jest-environment node
+ */
+
+import {
+  RulesTestEnvironment,
+  initializeTestEnvironment,
+} from '@firebase/rules-unit-testing';
+import { CommentWithRepliesT, DBPuzzleT } from '../lib/dbtypes.js';
+import {
+  overrideFirestore,
+  overrideToFirestore,
+} from '../lib/firebaseAdminWrapper.js';
+import { convertTimestamps, converter } from '../lib/firebaseWrapper.js';
+import { getMockedPuzzle } from '../lib/getMockedPuzzle.js';
+import { newPuzzleNotification } from '../lib/notificationTypes.js';
+import { handlePuzzleUpdate } from '../lib/puzzleUpdate.js';
+import { Timestamp } from '../lib/timestamp.js';
+
+const toDeleteId = 'puzzletodelete';
+const toKeepId = 'puzzletokeep';
+const baseTime = new Date(Date.UTC(2020, 10, 10));
+const baseTime2 = new Date(Date.UTC(2020, 11, 11));
+const basePuzzle = getMockedPuzzle({
+  cs: undefined,
+  f: true,
+  p: Timestamp.fromDate(baseTime),
+  pvu: Timestamp.fromDate(baseTime),
+});
